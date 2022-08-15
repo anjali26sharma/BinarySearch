92e4bc8cb713f5695e82b4ba55439bbb0cf687e3
anjali26sharma <79824809+anjali26sharma@users.noreply.github.com>
2022-08-15 17:07:18 +0530

    Create SearchElementInSortedMatrix.cpp

---
+++ SearchElementInSortedMatrix.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n-1;
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target)
+                return true;
+            else if(matrix[i][j]<target)
+                i++;
+            else if(matrix[i][j]>target)
+                j--;  
+        }
+        return false;
+    }
+};
